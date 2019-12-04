63ccb8f033d72ea9afac0e8c7ee1490e5c6abf8d
nizarRpl <ahmad.rpl001@gmail.com>
2019-12-05 00:47:36 +0700

    menu perpustakaan

---
+++ Perpustakaan/application/views/menu/peminjaman.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="author" content="Nasution">
+	<?php $this->load->view("_isi/head.php"); ?>
+</head>
+
+<body>
+	<?php $this->load->view("_isi/navbar.php"); ?>
+	<div class="container" style="margin-top: 80px">
+    <div class="row">
+        <?php $this->load->view("_isi/menu.php"); ?>
+        <div class="col-md-9">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-dashboard"></i> Peminjaman Buku</h3>
+              </div>
+              <div class="panel-body">
+
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
+	<?php $this->load->view("_isi/footer.php"); ?>
+	<?php  $this->load->view("_isi/js.php");?>
+</body>
+</html>
