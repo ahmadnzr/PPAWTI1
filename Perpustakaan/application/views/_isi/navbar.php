b160a37017c7b0bba2898baa811a940bb2ed0298
nizarRpl <ahmad.rpl001@gmail.com>
2019-12-04 23:17:59 +0700

    new navbar

--- Perpustakaan/application/views/_isi/navbar.php
+++ Perpustakaan/application/views/_isi/navbar.php
@@ -1,36 +1,17 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Perpustakaan STMIK Akakom Yogykarta</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Blog</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Menus
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Buku</a>
-          <a class="dropdown-item" href="#">Peminjaman</a>
-          <a class="dropdown-item" href="#">Pengembalian</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Lihat Update Buku Terbaru</a>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Perpustakaan STMIK AKAKOM YOGYAKARTA</a>
         </div>
-      </li>
-    </ul>
-    <form class="navbar-form navbar-right">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
-<br>
-
+        <div id="navbar" class="navbar-collapse collapse">
+            <div class="navbar-form navbar-right">
+                <a href="#" type="submit" class="btn btn-success"><i class="fa fa-sign-out"></i> Logout</a>
+            </div>
+      </div>
+    </nav>
