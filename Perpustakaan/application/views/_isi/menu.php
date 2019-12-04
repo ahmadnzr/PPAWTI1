96d7974371c625979cf8b874ac1cfdf12347555b
nizarRpl <ahmad.rpl001@gmail.com>
2019-12-05 00:46:50 +0700

    menambah menu makan

---
+++ Perpustakaan/application/views/_isi/menu.php
@@ -0,0 +1,13 @@
+<div class="col-md-3">
+    <div class="list-group">
+      <a href="#" class="list-group-item active" style="text-align: center;background-color: black;border-color: black">
+        ADMINISTRATOR
+      </a>
+      <a href="<?php echo site_url('Perpustakaan/index')?>" class="list-group-item"><i class="fa fa-dashboard"></i> Dashboard</a>
+      <a href="<?php echo site_url('Perpustakaan/Peminjaman')?>" class="list-group-item"><i class="fa fa-book"></i> Peminjaman</a>
+      <a href="<?php echo site_url('Perpustakaan/Pengembalian')?>" class="list-group-item"><i class="fa fa-book"></i> Pengembalian</a>
+      <a href="<?php echo site_url('Perpustakaan/dataBuku')?>" class="list-group-item"><i class="fa fa-book"></i> Buku</a>
+      <a href="<?php echo site_url('Perpustakaan/user')?>" class="list-group-item"><i class="fa fa-address-book"></i> Mahasiswa</a>
+      <a href="logout.php" class="list-group-item"><i class="fa fa-sign-out"></i> Logout</a>
+    </div>
+</div>
