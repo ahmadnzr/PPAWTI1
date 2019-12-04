cd785f26e8607d8f7c4707f222c67338fded31a7
nizarRpl <ahmad.rpl001@gmail.com>
2019-12-05 00:43:15 +0700

    Menu tambah buku

--- Perpustakaan/application/views/actions/tambah-buku.php
+++ Perpustakaan/application/views/actions/tambah-buku.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <?php $this->load->view("_isi/head.php"); ?>
-    </head>
-    <body>
-        <?php $this->load->view("_isi/navbar.php"); ?>
-        <div class="container">
-            <div class="col-md-9">
-                <h3>Tambah data Buku</h3>
-                <hr>
+<html lang="en">
+<head>
+	<meta name="author" content="Nasution">
+	<?php $this->load->view("_isi/head.php"); ?>
+</head>
+
+<body>
+	<?php $this->load->view("_isi/navbar.php"); ?>
+	<div class="container" style="margin-top: 80px">
+    <div class="row">
+        <?php $this->load->view("_isi/menu.php"); ?>
+        <div class="col-md-9">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-book"></i> Tambah Data Buku</h3>
+              </div>
+              <div class="panel-body">
                 <form action="<?php echo site_url('Perpustakaan/simpanData') ?>" method = "post">
                     <div class = "form-group">
                         <label>Kode Buku</label>
-                        <input type="text" name="kode_buku" class="form-control">
+                        <input type="text" name="kode_buku" class="form-control" size="10">
                     </div>
                     <div class = "form-group">
                         <label>Judul Buku</label>
@@ -38,13 +45,15 @@
                             <?php endforeach; ?>
                         </select>
                     </div>
-                   
+
                     <input type="submit" class="btn btn-success" value="Simpan">
                 </form>
+              </div>
             </div>
         </div>
-        <?php $this->load->view("_isi/footer.php"); ?>
-        <!--javascript-->
-        <?php $this->load->view("_isi/js.php"); ?>
-    </body>
+    </div>
+</div>
+	<?php $this->load->view("_isi/footer.php"); ?>
+	<?php  $this->load->view("_isi/js.php");?>
+</body>
 </html>
