dfaaf2b68f0f27743ebf312cad807e400cac37ae
nizarRpl <ahmad.rpl001@gmail.com>
2019-12-05 00:45:49 +0700

    Routes

--- Perpustakaan/application/controllers/Perpustakaan.php
+++ Perpustakaan/application/controllers/Perpustakaan.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Perpustakaan extends Ci_Controller{
-	
+
 	public function __construct(){
 		parent:: __construct();
 		$this->load->model("ModBuku");
@@ -10,6 +10,26 @@ class Perpustakaan extends Ci_Controller{
 	public function index(){
 		$data["tb_buku"] = $this->ModBuku->getTable();
 		$this->load->view("ruang.php", $data);
+	}
+	public function Peminjaman(){
+			//$data["tb_buku"] = $this->ModBuku->getTable();
+			$this->load->view('menu/peminjaman.php');
+		}
+	public function Pengembalian(){
+				//$data["tb_buku"] = $this->ModBuku->getTable();
+				$this->load->view('menu/pengembalian.php');
+		}
+	public function user(){
+			//$data["tb_buku"] = $this->ModBuku->getTable();
+					$this->load->view('menu/user.php');
+				}
+	public function Logout(){
+
+		$this->load-view('actions/logout.php');
+	}
+	public function dataBuku(){
+		$data["tb_buku"] = $this->ModBuku->getTable();
+		$this->load->view('menu/buku.php',$data);
 	}
 	public function tambahData(){
 		$this->load->model('ModKategori');
@@ -24,7 +44,7 @@ class Perpustakaan extends Ci_Controller{
 		$penerbit = $this->input->post('penerbit');
 		$tahun_terbit = $this->input->post('tahun_terbit');
 		$id_kategori = $this->input->post('id_kategori');
-		
+
 		$data = [
 			'kode_buku' => $kode,
 			'judul' => $judul,
