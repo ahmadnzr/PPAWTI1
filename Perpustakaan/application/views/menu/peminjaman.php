7db683781eb2086ad680f340a7ffe238a4ff87ab
nizarRpl <ahmad.rpl001@gmail.com>
2019-12-05 19:50:35 +0700

    Peminjaman Views

--- Perpustakaan/application/views/menu/peminjaman.php
+++ Perpustakaan/application/views/menu/peminjaman.php
@@ -16,7 +16,45 @@
                 <h3 class="panel-title"><i class="fa fa-dashboard"></i> Peminjaman Buku</h3>
               </div>
               <div class="panel-body">
-
+								<table class="table">
+									<div>
+										<a href="#" class="fa fa-plus-circle">Tambah Data Peminjaman</a>
+									</div>
+									<?php if ($this->session->flashdata('msg_success')):?>
+										<div class="alert alert-success"><<?php echo $this->session->flashdata('msg_success'); ?>
+										</div>
+									<?php endif ?>
+									<?php if ($this->session->flashdata('msg_error')):?>
+										<div class="alert alert-success"><<?php echo $this->session->flashdata('msg_error'); ?>
+										</div>
+									<?php endif ?>
+									<thead class="thead_dark">
+										<tr>
+											<th scope="col">Id</th>
+											<th scope="col">Tanggal Peminjaman</th>
+											<th scope="col">Buku</th>
+											<th scope="col">Mahasiswa</th>
+											<th scope="col">Aksi</th>
+										</tr>
+									</thead>
+									<tbody>
+										<?php
+											$no = 1;
+											foreach ($tb_peminjaman as $peminjaman):
+										?>
+										<tr>
+											<td><?php echo $no++ ?></td>
+											<td><?php echo $peminjaman->tgl ?></td>
+											<td><?php echo $peminjaman->judul ?></td>
+											<td><?php echo $peminjaman->nama ?></td>
+											<td>
+												<a href="#" class="fa fa-pencil-square-o"> Edit </a>&nbsp;&nbsp;&nbsp;
+									 			<a href="#" class="fa fa-trash-o" onclick="return confirm('Apakah kamu yakin?')"> Hapus</a>
+											</td>
+								 		</tr>
+										<?php endforeach ?>
+									</tbody>
+								</table>
               </div>
             </div>
         </div>
