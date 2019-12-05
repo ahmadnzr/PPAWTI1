185e9dba0b5eeb6fc3c63dca3319d250c4a71c91
nizarRpl <ahmad.rpl001@gmail.com>
2019-12-05 19:49:32 +0700

    Peminjaman Modul

--- Perpustakaan/application/models/ModPeminjaman.php
+++ Perpustakaan/application/models/ModPeminjaman.php
@@ -8,23 +8,24 @@ class ModPeminjaman extends CI_Model
 {
 
 	public function getTable(){
-		$this->db->select('tb_buku.*, tb_kategori.nama');
-		$this->db->from('tb_buku');
-		$this->db->join('tb_kategori', 'tb_kategori.id = tb_buku.id_kategori');
+		$this->db->select('tb_peminjaman.*, tb_mahasiswa.nama, tb_buku.id,tb_buku.judul');
+		$this->db->from('tb_peminjaman');
+		$this->db->join('tb_buku', 'tb_buku.id = tb_peminjaman.buku_id');
+		$this->db->join('tb_mahasiswa', 'tb_mahasiswa.id = tb_peminjaman.mahasiswa_id');
 		$query = $this->db->get();
 		return $query->result();
 	}
 	public function insert($data){
-		return $this->db->insert('tb_buku', $data);
+		return $this->db->insert('tb_peminjaman', $data);
 	}
 	public function get($id){
-		return $this->db->where('id', $id)->get('tb_buku')->row();
+		return $this->db->where('id', $id)->get('tb_peminjaman')->row();
 	}
 	public function update($data, $id){
-		return $this->db->where('id',$id)->update('tb_buku',$data);
+		return $this->db->where('id',$id)->update('tb_peminjaman',$data);
 	}
 	public function delete($id){
-		return $this->db->where('id',$id)->delete('tb_buku');
+		return $this->db->where('id',$id)->delete('tb_peminjaman');
 	}
 }
  ?>
