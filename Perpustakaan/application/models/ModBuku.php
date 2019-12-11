f9ac29d4e4e934e358ae5b60ae702628af28dcbd
nizarRpl <ahmad.rpl001@gmail.com>
2019-12-11 23:16:28 +0700

    Modul Buku

--- Perpustakaan/application/models/ModBuku.php
+++ Perpustakaan/application/models/ModBuku.php
@@ -6,7 +6,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class ModBuku extends CI_Model
 {
-
+	public function getAll()
+	{
+		return $this->db->get('tb_buku')->result();
+	}
 	public function getTable(){
 		$this->db->select('tb_buku.*, tb_kategori.nama');
 		$this->db->from('tb_buku');
